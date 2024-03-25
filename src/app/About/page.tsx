41d302caf488a0614461301083a435e1fc4a6bfb
Asadullah <asadullahasad942@gmail.com>
2024-03-26 02:09:53 +0500

    first commit

---
+++ src/app/About/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+function About(){
+    return( 
+        <body><nav className="px-4 py-4 bg-blue-700">
+        <ul className="flex space-x-5 justify-end">
+          <li className="text-white font-bold"><Link href={'/Home'}>Home</Link></li>
+          <li className="text-white font-bold"><Link href={'/Contact-us'}>Contact-us</Link></li>
+          <li className="text-white font-bold"><Link href={'/About'}>About</Link></li>
+        </ul>
+        </nav>
+    <div className="bg-blue-400 h-96"><p className="text-3xl text-center align-middle p-32">This is About page</p></div></body>
+)}
+export default About;
